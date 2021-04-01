8182958216657093d438638edab7c618125aca87
n.skorokhod <mtwain.mark@gmail.com>
2021-04-01 22:37:43 +0300

    Комментирование метода определения ошибок

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math
 
-from math import sin, cos, atan2, pi, fabs
-
-from matplotlib.patches import Ellipse
-
 
 def ellipse_cloud(x0, y0, rx, ry, npoints, noise, pivot, regular=True):
     if regular:
@@ -29,20 +25,30 @@ def ellipse_cloud(x0, y0, rx, ry, npoints, noise, pivot, regular=True):
     return x_pivot + pogx, y_pivot + pogy
 
 
+# https://wet-robots.ghost.io/simple-method-for-distance-to-ellipse/
 def solve(semi_major, semi_minor, p):
+    # координаты точек по модулю
     px = abs(p[0])
     py = abs(p[1])
 
-    tx = 0.707
-    ty = 0.707
+    # 0.7854
+    # atan2(py, px)
+    # предполагается, когда p находится внутри эллипса оно возвращает неверные значения
+    tx = math.pi / 4
+    ty = math.pi / 4
 
+    # центр эллипса
     a = semi_major
     b = semi_minor
 
     for x in range(0, 3):
+        # уравнение эллипса
         x = a * tx
         y = b * ty
 
+        # Эволююта плоской кривой — геометрическое место точек, являющихся центрами кривизны кривой.
+        # По отношению к своей эволюте любая кривая является эвольвентой.
+        # параметрическая форма
         ex = (a * a - b * b) * tx ** 3 / a
         ey = (b * b - a * a) * ty ** 3 / b
 
@@ -52,15 +58,19 @@ def solve(semi_major, semi_minor, p):
         qx = px - ex
         qy = py - ey
 
+        # возвращает квадратный корень суммы квадратов своих аргументов
         r = math.hypot(ry, rx)
         q = math.hypot(qy, qx)
 
+        # сравниваем и присваеваем минимальное из 1 и из максимального
         tx = min(1, max(0, (qx * r / q + ex) / a))
         ty = min(1, max(0, (qy * r / q + ey) / b))
+        # возвращает квадратный корень суммы квадратов своих аргументов
         t = math.hypot(ty, tx)
+
         tx /= t
         ty /= t
-
+    # copysign - возвращает значение первого аргумента и знак второго аргумента
     return math.copysign(a * tx, p[0]), math.copysign(b * ty, p[1])
 
 
@@ -71,15 +81,18 @@ if __name__ == '__main__':
     ry = 14
     pivot = 10
 
+    # построение точек
     x, y = ellipse_cloud(x0, y0, rx, ry, 77, 0.3, pivot)
-    xe, ye = ellipse_cloud(x0, y0, rx, ry, 500, 0, pivot)
+    # построение эллипса
+    xe, ye = ellipse_cloud(x0, y0, rx, ry, 77, 0, pivot)
 
     x_normal_error_out, y_normal_error_out = [], []
 
     for i in range(len(x)):
+        # Вычисление ошибки для каждой точки
         x_normal_error, y_normal_error = solve(x0, y0, [x[i], y[i]])
-        x_normal_error_out.append(x_normal_error + x[i])
-        y_normal_error_out.append(y_normal_error + y[i])
+        x_normal_error_out.append(x[i] + x_normal_error)
+        y_normal_error_out.append(y[i] + y_normal_error)
 
     plt.plot(x_normal_error_out, y_normal_error_out, 'r')
 
